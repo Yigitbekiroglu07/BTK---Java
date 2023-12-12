f6cc20293bc276205d949ae06864f25dd0243845
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2023-12-12 10:46:41 +0300

    Java Uygulamaları

---
+++ Java Uygulamaları/Interface/static/DataBaseHelper.java
@@ -0,0 +1,19 @@
+public class DataBaseHelper {
+
+    public static class Crud{ //Create read update delete
+        public static void delete(){
+
+        }
+        public static void Update(){
+
+        }
+        public static class Connection{
+            public static void createConnection(){
+
+        }
+
+
+        }
+
+    }
+}
